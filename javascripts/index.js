cbbce37ca04fedfbb5f95a8d374ae855c226e043
Kevin <kjeong0@gmail.com>
2014-12-04 23:52:46 -0500

    super cereal

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,20 +1,18 @@
 $(document).ready(function () {
 	$(window).scroll(function (evt) {
-		var opacity = 1- $(window).scrollTop()/($('#overlay').height()*3);
+		var opacity = 1- $(window).scrollTop()/($('#overlay-white').height()*3);
+		hundredEverything();
 		if (opacity >= 0) {
-			$('#overlay').css('opacity', opacity);
+			$('#overlay-white').css('opacity', opacity);
 			$('#name').css('opacity', 1-opacity);
 			$('#b-t').css('width', (100-opacity * 100)+'%');
 			$('#b-b').css('width', (100-opacity * 100)+'%');
 			$('#b-r').css('height', (100-opacity * 100)+'%');
 			$('#b-l').css('height', (100-opacity * 100)+'%');
 			$('#name-center').css('top', '50%');
-		} else if ($(window).scrollTop() > $('#overlay').height()*5){
-			hundredEverything();
-			var goUp = ($(window).scrollTop() - ($('#overlay').height()*5)) /2;
+		} else if ($(window).scrollTop() > $('#overlay-white').height()*5){
+			var goUp = ($(window).scrollTop() - ($('#overlay-white').height()*5)) /2;
 			$('#name-center').css('top', ($(window).height()/2) - goUp);
-		} else {
-			hundredEverything();
 		}
 	});
 });
@@ -24,7 +22,7 @@ function hundredEverything () {
 	$('#b-b').css('width', '100%');
 	$('#b-l').css('height', '100%');
 	$('#b-r').css('height', '100%');
-	$('#overlay').css('opacity', 0);
+	$('#overlay-white').css('opacity', 0);
 	$('#name').css('opacity', 1);
 	$('#name-center').css('top', '50%');
 }
