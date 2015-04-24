c95fb499c46d27daaa90db0dd29dc98e71bdca26
Kevin Jeong <kevin@Kevins-MacBook-Pro.local>
2015-04-23 20:48:10 -0400

    Under construction

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-	console.log('test');
+	setTimeout(function () {
+		$('#loading').css('opacity', 0);
+	}, 200);
+	setTimeout(function () {
+		$('#loading').css('display', 'none');
+	}, 1200);
+
+	$( window ).scroll (function () {
+		var height = $(window).height();
+		var scrollTop = $(window).scrollTop();
+		var calc = scrollTop/height;
+		if (calc < 1){
+			if (calc >= 0.6){
+				calc -= 0.6;
+				calc /= 0.4;
+				$('#parallax_cover').css('opacity', calc);
+				return;
+			}
+		}
+		$('#parallax_cover').css('opacity', 0);
+	})
 });
