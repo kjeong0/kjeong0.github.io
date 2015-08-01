948ac8a574641d105885c1af2d0fbf0449ea420d
Kevin Jeong <kevin@Kevins-MacBook-Pro.local>
2015-07-31 23:17:24 -0400

    new shit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
 
 });
-
 $(window).load(function () {
 	var height = $(window).height();
 	var scrollTop = $(window).scrollTop();
 	var calc = scrollTop/height;
+
+	createCloud();
+	cloudRecursion();
 
 	if (height < scrollTop) {
 		$('.header').css('opacity', 1);
@@ -57,3 +59,53 @@ $(window).load(function () {
 		}
 	});
 });
+
+function cloudRecursion () {
+	var recursionSpeed = Math.floor(($(document).width()/$(window).width())*900);
+	setTimeout(function() {
+		if($(window).scrollTop() <= $(window).height()){
+			createCloud();
+		}
+		cloudRecursion();
+	}, recursionSpeed);
+}
+
+function createCloud () {
+	//TO DO: make this work for jQuery
+	var cloud = document.createElement('div');
+	cloud.className = 'cloud';
+
+	var cloudJquery = $(cloud);
+	var height = Math.floor(Math.random()*100) + 100;
+	var width = height*2;
+	cloudJquery.css('width', width);
+	cloudJquery.css('height', height);
+
+	var topPosition = Math.floor(Math.random()*($(window).height()-height));
+	cloudJquery.css('top', topPosition);
+
+	$('#clouds').append(cloud);
+
+	var speed = Math.floor(Math.random()*10) + 10;
+	if(height%2 == 0){
+		TweenLite.set(cloudJquery, {right: -width})
+		TweenLite.from(cloudJquery, speed, {right: width+$(window).width()})
+	} else {
+		TweenLite.set(cloudJquery, {left: -width})
+		TweenLite.from(cloudJquery, speed, {left: width+$(window).width()})
+	}
+	var cloud_id = 1
+	if(speed%2 == 0){
+		cloud_id = 2;
+	}
+	cloudJquery.css('background-image', 'url(\'assets/cloud_temp_'+cloud_id+'.png\')');
+
+
+	setTimeout(function() {
+		removeCloud(cloud);
+	}, speed*1000);
+}
+
+function removeCloud (cloud) {
+	cloud.remove();
+}
